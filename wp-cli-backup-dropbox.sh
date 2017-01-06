f68443f11c2ad4a40fbbbde99bcc229a9fb08b51
WP Bullet <wpbulletplugins@gmail.com>
2017-01-06 16:10:12 +0100

    Update wp-cli-backup-dropbox.sh
    update gzip method to not break things

--- wp-cli-backup-dropbox.sh
+++ wp-cli-backup-dropbox.sh
@@ -43,7 +43,8 @@ for SITE in ${SITELIST[@]}; do
 
     #back up the WordPress database
     wp db export $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql --allow-root --skip-themes --skip-plugins
-    tar -czf $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql.gz $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql
+    #tar -czf $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql.gz $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql
+    cat $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql | gzip > $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql.gz
     rm $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql
 
     #upload packages
