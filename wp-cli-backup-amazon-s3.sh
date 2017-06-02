f3d27cd434168f0f337d871f46d22645420e679f
WP Bullet <wpbulletplugins@gmail.com>
2017-06-02 08:22:35 -0400

    Update wp-cli-backup-amazon-s3.sh
    remove tar of sql

--- wp-cli-backup-amazon-s3.sh
+++ wp-cli-backup-amazon-s3.sh
@@ -41,7 +41,6 @@ for SITE in ${SITELIST[@]}; do
 
     #back up the WordPress database
     wp db export $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql --path=$SITESTORE/$SITE --single-transaction --quick --lock-tables=false --allow-root --skip-themes --skip-plugins
-    #tar -czf $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql.gz $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql
     cat $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql | gzip > $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql.gz
     rm $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql
 
