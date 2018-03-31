eeaa9e7aa2e741ae6d64824b8510cbef449e5238
WP Bullet <wpbulletplugins@gmail.com>
2018-03-31 11:08:47 -0400

    Update no-wp-cli.sh
    remove escaped slash from no-wp-cli script

--- no-wp-cli.sh
+++ no-wp-cli.sh
@@ -9,10 +9,10 @@ WPPATH="/var/www/domain.com"
 WPCONFIG="$WPPATH/wp-config.php"
 
 # extract database host and credentials
-DBHOST=$(grep DB_HOST "$WPCONFIG" | awk -F ["\'"] '{ print $4 }')
-DBUSER=$(grep DB_USER "$WPCONFIG" | awk -F ["\'"] '{ print $4 }')
-DBPASS=$(grep DB_PASSWORD "$WPCONFIG" | awk -F ["\'"] '{ print $4 }')
-DBNAME=$(grep DB_NAME "$WPCONFIG" | awk -F ["\'"] '{ print $4 }')
+DBHOST=$(grep DB_HOST "$WPCONFIG" | awk -F ["'"] '{ print $4 }')
+DBUSER=$(grep DB_USER "$WPCONFIG" | awk -F ["'"] '{ print $4 }')
+DBPASS=$(grep DB_PASSWORD "$WPCONFIG" | awk -F ["'"] '{ print $4 }')
+DBNAME=$(grep DB_NAME "$WPCONFIG" | awk -F ["'"] '{ print $4 }')
 
 # mysqldump optimized for speed and gzip
 mysqldump -h "$DBHOST" -u "$DBUSER" -p"$DBPASS" "$DBNAME" --single-transaction --quick --lock-tables=false | gzip > /tmp/latest.sql.gz
